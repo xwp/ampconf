9eda6ba3c85c0c8cee78c784c43b82975fcdb1f0
Davis Shaver <davisshaver@gmail.com>
2018-09-26 15:21:13 -0400

    Add filter to change author prefix

--- templates/entry/meta/byline.php
+++ templates/entry/meta/byline.php
@@ -8,9 +8,11 @@
 ?>
 <span class="entry__meta entry__meta--byline">
 	<?php
+	/* translators: %s: post author. */
+	$default = _x( 'by', 'post author', 'ampnews' );
 	printf(
-		/* translators: %s: post author. */
-		esc_html_x( 'by %s', 'post author', 'ampnews' ),
+		esc_html( '%s %s' ),
+		apply_filters( 'ampnews_filter_author_prefix', $default ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 	?>
