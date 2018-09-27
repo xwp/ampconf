3c0ad51f775fce75f11577794cafbb8af3e1b7f9
Davis Shaver <davisshaver@gmail.com>
2018-09-27 16:27:08 -0400

    Fix attr new

--- templates/entry/featured.php
+++ templates/entry/featured.php
@@ -9,7 +9,7 @@
 <article <?php ampnews_the_post_attributes( array( 'class' => 'entry wrap__item' ) ); ?>>
 	<?php if ( has_post_thumbnail() ) : ?>
 		<figure class="entry__thumbnail">
-			<a href="<?php the_permalink(); ?>">
+			<a href="<?php the_permalink(); ?>" <?php echo ampnews_permalink_open_new(); ?>>
 				<?php the_post_thumbnail( 'ampnews-1040x585', array( 'data-amp-layout' => 'fill' ) ); ?>
 			</a>
 		</figure><!-- .entry__thumbnail -->
@@ -17,7 +17,7 @@
 
 	<header class="entry__header">
 		<?php get_template_part( 'templates/entry/meta/date' ); ?>
-		<?php the_title( '<h2 class="entry__title"><a href="' . esc_url( get_permalink() ) . '" ' . ampnews_permalink_open_new() . 'rel="bookmark">', '</a></h2>' ); ?>
+		<?php the_title( '<h2 class="entry__title"><a href="' . esc_url( get_permalink() ) . '" ' . ampnews_permalink_open_new() . ' rel="bookmark">', '</a></h2>' ); ?>
 	</header><!-- .entry__header -->
 
 	<div class="entry__summary">
