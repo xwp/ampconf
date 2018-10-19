e0506c63ace8f529b1167386eda8cdb6219934e1
Davis Shaver <davisshaver@gmail.com>
2018-10-19 17:00:24 -0400

    Drop social from pages

--- templates/entry/full-page.php
+++ templates/entry/full-page.php
@@ -19,15 +19,5 @@
 
 	<div class="entry__content">
 		<?php the_content(); ?>
-
-		<div class="entry__share entry__share--in-content">
-			<?php get_template_part( 'templates/social-share' ); ?>
-		</div>
 	</div><!-- .entry__summary -->
-
-	<footer class="entry__footer">
-		<div class="entry__share entry__share--in-footer">
-			<?php get_template_part( 'templates/social-share' ); ?>
-		</div>
-	</footer><!-- .entry__footer -->
 </article><!-- .entry -->
