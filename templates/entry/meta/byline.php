ebb5a03cfa4310c2261eba6f779c911ae29937d5
Davis Shaver <davisshaver@gmail.com>
2019-04-11 09:32:00 -0400

    Fix byline issue

--- templates/entry/meta/byline.php
+++ templates/entry/meta/byline.php
@@ -14,7 +14,7 @@
 	printf(
 		esc_html( '%s %s %s' ),
 		apply_filters( 'ampnews_filter_author_prefix', $default ),
-		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>',
+		'<span class="author vcard"><a '. ampnews_permalink_open_new() . ' class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>',
 		apply_filters( 'ampnews_filter_author_suffix', $default_suffix )
 	);
 	?>
